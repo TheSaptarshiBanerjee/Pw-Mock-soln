dfb15f42caf61f31d2ea88ad7666f5e797ef934b
Saptarshi Banerjee <90561422+TheSaptarshiBanerjee@users.noreply.github.com>
2023-07-03 12:15:37 +0530

    Create VariableDeclaration.java

---
+++ VariableDeclaration.java
@@ -0,0 +1,13 @@
+// Write a Java program to declare and initialize variables of different data types (int, double, String) with appropriate identifiers.
+
+public class VariableDeclaration {
+    public static void main(String[] args) {
+        int age = 21;
+        double weight = 73.5;
+        String name = "Saptarshi Banerjee";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Weight: " + weight);
+    }
+}
